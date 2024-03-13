9843325b21f0f65401bf837e1e75c4da0c6913e9
Leonid Kolesnichenko <leonid.kolesnichenko@nextgis.com>
2024-03-13 17:55:01 +0300

    Add lnglat ufunc

--- vect_rcantile/__init__.py
+++ vect_rcantile/__init__.py
@@ -1,10 +1,10 @@
 import numpy as np
 from numba import guvectorize, float64
 
-from vect_rcantile.constants import RE
+from vect_rcantile.constants import RE, R2D
 
 
-@guvectorize([(float64[:], float64[:], float64[:], float64[:])], '(n),(n)->(n),(n)', target='parallel')
+@guvectorize([(float64[:], float64[:], float64[:], float64[:])], '(n),(n)->(n),(n)', nopython=True, target='parallel')
 def truncate_lnglat(lngs, lats, out_lngs, out_lats):
     for i in range(lngs.shape[0]):
         # Truncate longitudes: directly modifying the output array
@@ -35,3 +35,10 @@ def xy(lngs, lats, out_xs, out_ys):
             out_ys[i] = np.inf
         else:
             out_ys[i] = RE * np.log(np.tan((np.pi * 0.25) + (0.5 * np.radians(lats[i]))))
+
+
+@guvectorize([(float64[:], float64[:], float64[:], float64[:])], '(n),(n)->(n),(n)', target='parallel')
+def lnglat(xs, ys, out_lngs, out_lats):
+    for i in range(xs.shape[0]):
+        out_lngs[i] = xs[i] * R2D / RE
+        out_lats[i] = ((np.pi * 0.5) - 2.0 * np.arctan(np.exp(-ys[i] / RE))) * R2D
