d084d51c579de96390c86a89487456a39c232073
Leonid Kolesnichenko <xperience439@gmail.com>
2024-03-13 21:45:59 +0300

    Revamp the ufuncs

--- vect_rcantile/__init__.py
+++ vect_rcantile/__init__.py
@@ -1,69 +1,98 @@
+import math
+
 import numpy as np
 from numba import guvectorize, float64, int64
 
-from vect_rcantile.constants import RE, R2D
+from vect_rcantile.constants import RE, R2D, CE, EPSILON
 
 
-@guvectorize([(float64[:], float64[:], float64[:], float64[:])], '(n),(n)->(n),(n)', nopython=True, target='parallel')
-def truncate_lnglat(lngs, lats, out_lngs, out_lats):
-    for i in range(lngs.shape[0]):
-        # Truncate longitudes: directly modifying the output array
-        if lngs[i] > 180.0:
-            out_lngs[i] = 180.0
-        elif lngs[i] < -180.0:
-            out_lngs[i] = -180.0
-        else:
-            out_lngs[i] = lngs[i]
-
-        # Truncate latitudes: similarly, directly modifying the output array
-        if lats[i] > 90.0:
-            out_lats[i] = 90.0
-        elif lats[i] < -90.0:
-            out_lats[i] = -90.0
-        else:
-            out_lats[i] = lats[i]
-
-
-@guvectorize([(float64[:], float64[:], float64[:], float64[:])], '(n),(n)->(n),(n)', nopython=True, target='parallel')
-def xy(lngs, lats, out_xs, out_ys):
-    for i in range(lngs.shape[0]):
-        out_xs[i] = RE * np.radians(lngs[i])
-
-        if lats[i] <= -90:
-            out_ys[i] = -np.inf
-        elif lats[i] >= 90:
-            out_ys[i] = np.inf
-        else:
-            out_ys[i] = RE * np.log(np.tan((np.pi * 0.25) + (0.5 * np.radians(lats[i]))))
-
-
-@guvectorize([(float64[:], float64[:], float64[:], float64[:])], '(n),(n)->(n),(n)', nopython=True, target='parallel')
-def lnglat(xs, ys, out_lngs, out_lats):
-    for i in range(xs.shape[0]):
-        out_lngs[i] = xs[i] * R2D / RE
-        out_lats[i] = ((np.pi * 0.5) - 2.0 * np.arctan(np.exp(-ys[i] / RE))) * R2D
-
-
-@guvectorize([(int64[:], int64[:], int64[:], float64[:], float64[:])], '(n),(n),(n)->(n),(n)', nopython=True,
-             target='parallel')
-def ul(xs, ys, zooms, out_lngs, out_lats):
-    for i in range(zooms.shape[0]):
-        Z2 = zooms[i] ** 2
-        lon_deg_i = xs[i] / Z2 * 360.0 - 180.0
-        lat_rad_i = np.arctan(np.sinh(np.pi * (1 - 2 * ys[i] / Z2)))
-        lat_deg_i = np.degrees(lat_rad_i)
-        out_lngs[i] = lon_deg_i
-        out_lats[i] = lat_deg_i
-
-
-@guvectorize([(int64[:], int64[:], int64[:], float64[:], float64[:], float64[:], float64[:])],
-             '(n),(n),(n)->(n),(n),(n),(n)',
+@guvectorize([(float64, float64, float64[:], float64[:])],
+             '(),()->(),()',
              nopython=True,
              target='parallel')
-def bounds(xs, ys, zooms, ul_lons_deg, lr_lats_deg, lr_lons_deg, ul_lats_deg):
-    for i in range(zooms.shape[0]):
-        Z2 = zooms[i] ** 2
-        ul_lons_deg[i] = xs[i] / Z2 * 360.0 - 180.0
-        lr_lats_deg[i] = np.degrees(np.arctan(np.sinh(np.pi * (1 - 2 * (ys[i] + 1) / Z2))))
-        lr_lons_deg[i] = (xs[i] + 1) / Z2 * 360.0 - 180.0
-        ul_lats_deg[i] = np.degrees(np.arctan(np.sinh(np.pi * (1 - 2 * ys[i] / Z2))))
+def truncate_lnglat(lng, lat, out_lng, out_lat):
+    if lng > 180.0:
+        out_lng[0] = 180.0
+    elif lng < -180.0:
+        out_lng[0] = -180.0
+    else:
+        out_lng[0] = lng
+
+    # Truncate latitudes: similarly, directly modifying the output array
+    if lat > 90.0:
+        out_lat[0] = 90.0
+    elif lat < -90.0:
+        out_lat[0] = -90.0
+    else:
+        out_lat[0] = lat
+
+
+@guvectorize([(float64, float64, float64[:], float64[:])],
+             '(),()->(),()',
+             nopython=True,
+             target='parallel')
+def xy(lng, lat, out_xs, out_ys):
+    out_xs[0] = RE * np.radians(lng)
+    if lat <= -90:
+        out_ys[0] = -np.inf
+    elif lat >= 90:
+        out_ys[0] = np.inf
+    else:
+        out_ys[0] = RE * np.log(np.tan((np.pi * 0.25) + (0.5 * np.radians(lat))))
+
+
+@guvectorize([(float64, float64, float64[:], float64[:])],
+             '(),()->(),()',
+             nopython=True,
+             target='parallel')
+def lnglat(x, y, out_lngs, out_lats):
+    out_lngs[0] = x * R2D / RE
+    out_lats[0] = ((np.pi * 0.5) - 2.0 * np.arctan(np.exp(-y / RE))) * R2D
+
+
+@guvectorize([(int64, int64, int64, float64[:], float64[:])],
+             '(),(),()->(),()',
+             nopython=True,
+             target='parallel')
+def ul(x, y, zoom, out_lngs, out_lats):
+    Z2 = 2 ** zoom
+    lon_deg_i = x / Z2 * 360.0 - 180.0
+    lat_rad_i = np.arctan(np.sinh(np.pi * (1 - 2 * y / Z2)))
+    lat_deg_i = np.degrees(lat_rad_i)
+    out_lngs[0] = lon_deg_i
+    out_lats[0] = lat_deg_i
+
+
+@guvectorize([(int64, int64, int64, float64[:], float64[:], float64[:], float64[:])],
+             '(),(),()->(),(),(),()',
+             nopython=True,
+             target='parallel')
+def bounds(x, y, zoom, ul_lons_deg, lr_lats_deg, lr_lons_deg, ul_lats_deg):
+    Z2 = 2 ** zoom
+    ul_lons_deg[0] = x / Z2 * 360.0 - 180.0
+    lr_lats_deg[0] = np.degrees(np.arctan(np.sinh(np.pi * (1 - 2 * (y + 1) / Z2))))
+    lr_lons_deg[0] = (x + 1) / Z2 * 360.0 - 180.0
+    ul_lats_deg[0] = np.degrees(np.arctan(np.sinh(np.pi * (1 - 2 * y / Z2))))
+
+
+@guvectorize([(int64, int64, int64, float64[:], float64[:], float64[:], float64[:])],
+             '(),(),()->(),(),(),()',
+             nopython=True,
+             target='parallel')
+def xy_bounds(x, y, zoom, lefts, bottoms, rights, tops):
+    tile_size = CE / 2 ** zoom
+    lefts[0] = x * tile_size - CE / 2
+    rights[0] = lefts[0] + tile_size
+    tops[0] = CE / 2 - y * tile_size
+    bottoms[0] = tops[0] - tile_size
+
+
+@guvectorize([(float64, float64, float64[:], float64[:])],
+             '(),()->(),()',
+             nopython=True,
+             target='parallel')
+def _xy(lng, lat, xs, ys):
+    xs[0] = lng / 360.0 + 0.5
+    sinlat = np.sin(np.radians(lat))
+    ys[0] = 0.5 - 0.25 * np.log((1.0 + sinlat) / (1.0 - sinlat)) / np.pi
+
