6eaaad82778b0bc8ca5a42381676f6587550c8ee
Leonid Kolesnichenko <xperience439@gmail.com>
2024-03-14 22:45:02 +0300

    Add quadkey_to_tile Add generate_tiles

--- vect_rcantile/njits.py
+++ vect_rcantile/njits.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numba import float64, njit, prange
+from mercantile import ParentTileError, InvalidZoomError
+from numba import float64, int64, njit, prange
 
 from vect_rcantile import RE, R2D
 
@@ -63,4 +64,51 @@ def ul(x, y, zoom):
     lat_deg = np.degrees(lat_rad)
     result[0] = lon_deg
     result[1] = lat_deg
+    return result
+
+
+from vect_rcantile.constants import LL_EPSILON, EPSILON
+
+
+@njit('int64[:](float64,float64,int64)', parallel=True, fastmath=True)
+def tile(lng, lat, zoom):
+    result = np.zeros((3), dtype=int64)
+    x = lng / 360.0 + 0.5
+    y = 0.5 - 0.25 * np.log((1.0 + np.sin(np.radians(lat))) / (1.0 - np.sin(np.radians(lat)))) / np.pi
+    Z2 = 2 ** zoom
+    result[2] = zoom
+    if x <= 0:
+        result[0] = 0
+    elif x >= 1:
+        result[0] = np.int64(Z2 - 1)
+    else:
+        result[0] = np.int64(np.floor((x + EPSILON) * Z2))
+
+    if y <= 0:
+        result[1] = 0
+    elif y >= 1:
+        result[1] = np.int64(Z2 - 1)
+    else:
+        result[1] = np.int64(np.floor((y + EPSILON) * Z2))
+    return result
+
+
+@njit('int64[:,:](float64,float64,float64,float64,int64,boolean)', parallel=True, fastmath=True)
+def generate_tiles(w, s, e, n, zoom, truncate=False):
+    if truncate:
+        w, s = truncate_lnglat(w, s)
+        e, n = truncate_lnglat(e, n)
+
+    ul_tile = tile(w, n, zoom)
+    lr_tile = tile(e - LL_EPSILON, s + LL_EPSILON, zoom)
+
+    num_tiles = (lr_tile[0] - ul_tile[0] + 1) * (lr_tile[1] - ul_tile[1] + 1)
+
+    result = np.zeros((num_tiles, 3), dtype=np.int64)
+    index = 0
+    while index < num_tiles:
+        result[index][0] = index % (lr_tile[0] - ul_tile[0] + 1) + ul_tile[0]
+        result[index][1] = index // (lr_tile[0] - ul_tile[0] + 1) + ul_tile[1]
+        result[index][2] = zoom
+        index += 1
     return result
