dbbc14f84817176a62dd7b814afa1ff16b7cadab
Leonid Kolesnichenko <leonid.kolesnichenko@nextgis.com>
2024-03-14 19:09:37 +0300

    Add njits.ipynb

--- vect_rcantile/njits.py
+++ vect_rcantile/njits.py
@@ -6,49 +6,61 @@ from vect_rcantile import RE, R2D
 
 @njit('float64[:](float64, float64)', parallel=True, fastmath=True)
 def truncate_lnglat(lng, lat):
-    temp = np.zeros((2), dtype=float64)
+    result = np.zeros((2), dtype=float64)
     if lng > 180.0:
-        temp[0] = 180.0
+        result[0] = 180.0
     elif lng < -180.0:
-        temp[0] = -180.0
+        result[0] = -180.0
     else:
-        temp[0] = lng
+        result[0] = lng
 
     # Truncate latitudes: similarly, directly modifying the output array
     if lat > 90.0:
-        temp[1] = 90.0
+        result[1] = 90.0
     elif lat < -90.0:
-        temp[1] = -90.0
+        result[1] = -90.0
     else:
-        temp[1] = lat
-    return temp
+        result[1] = lat
+    return result
 
 
 @njit('float64[:](float64, float64, boolean)', parallel=True, fastmath=True)
 def xy(lng, lat, truncate=False):
-    temp = np.zeros((2), dtype=float64)
+    result = np.zeros((2), dtype=float64)
     if truncate:
         lng, lat = truncate_lnglat(lng, lat)
 
-    temp[0] = RE * np.radians(lng)
+    result[0] = RE * np.radians(lng)
 
     if lat <= -90:
-        temp[1] = -np.inf
+        result[1] = -np.inf
     elif lat >= 90:
-        temp[1] = np.inf
+        result[1] = np.inf
     else:
-        temp[1] = RE * np.log(np.tan((np.pi * 0.25) + (0.5 * np.radians(lat))))
+        result[1] = RE * np.log(np.tan((np.pi * 0.25) + (0.5 * np.radians(lat))))
 
-    return temp
+    return result
 
 
 @njit('float64[:](float64, float64, boolean)', parallel=True, fastmath=True)
 def lnglat(x, y, truncate=False):
-    temp = np.zeros((2), dtype=float64)
-    temp[0], temp[1] = (
+    result = np.zeros((2), dtype=float64)
+    result[0], result[1] = (
         x * R2D / RE,
         ((np.pi * 0.5) - 2.0 * np.arctan(np.exp(-y / RE))) * R2D,
     )
     if truncate:
-        temp[0], temp[1] = truncate_lnglat(temp[0], temp[1])
-    return temp
+        result[0], result[1] = truncate_lnglat(result[0], result[1])
+    return result
+
+
+@njit('float64[:](int64, int64, int64)', parallel=True, fastmath=True)
+def ul(x, y, zoom):
+    result = np.zeros((2), dtype=float64)
+    Z2 = 2 ** zoom
+    lon_deg = x / Z2 * 360.0 - 180.0
+    lat_rad = np.arctan(np.sinh(np.pi * (1 - 2 * y / Z2)))
+    lat_deg = np.degrees(lat_rad)
+    result[0] = lon_deg
+    result[1] = lat_deg
+    return result
